92fabf44695b77a33dd597a5d34ec645469000b1
SamaDawihi <90503910+SamaDawihi@users.noreply.github.com>
2023-05-16 13:23:25 +0300

    allocation method END

--- Memory.java
+++ Memory.java
@@ -25,52 +25,49 @@ class Memory{
         
     switch (allocationStrategy) {
         case FIRST_FIT:
-        FirstFitAllocate(PID,size);
-            break;
+            return FirstFitAllocate(PID,size);
         case BEST_FIT:
-            BestFitAllocate(PID,size);
-            break;
+            return BestFitAllocate(PID,size);
         case WORST_FIT:
-
-        break;
+            return WorstFit(PID, size);
         default:
-            // Invalid approach.
-         }
-    return true;
+            return false;
+        }
     }
     public boolean BestFitAllocate(String PID, int Psize){
-        int smallestFit = -1;
-
         if(PID == null) return false;
 
-        for(int i=0; i<noOfPartitions; i++; ){
+        int smallestFit = -1;
+
+        for(int i = 0; i < noOfPartitions; i++){
           if(partion[i].isFree() && partion[i].getSize() >= Psize ){
             smallestFit = i;
           
-          for(int j = i+1 ; j<noOfPartitions ; j++){
-             if(partion[j].isFree() && partion[j].getSize() >= Psize && partion[j].getSize() < partion[smallestFit].getSize() ){
-            smallestFit = j;
-             }
-          }
-          break;
+            for(int j = i+1 ; j<noOfPartitions ; j++){
+                if(partion[j].isFree() && partion[j].getSize() >= Psize && partion[j].getSize() < partion[smallestFit].getSize() ){
+                    smallestFit = j;
+                }
+            }
+            break;
           }
         } // end of outer loop
-        if(smallestFit != -1){
 
-                partion[smallestFit].setStatus(false);
-                partion[smallestFit].setProcessID(PID);
-                partion[smallestFit].CalcInternalFragment(Psize);
-                 return true;
+        if(smallestFit != -1){
+            partion[smallestFit].setStatus(false);
+            partion[smallestFit].setProcessID(PID);
+            partion[smallestFit].CalcInternalFragment(Psize);
+            return true;
         }
        return false;
     }
 
     private boolean FirstFitAllocate(String PID, int Psize){
-
         if(PID == null) return false;
+
         for(int i = 0; i < noOfPartitions; i++){
             if(Psize <= partion[i].getSize() && partion[i].isFree()){
-    
+
+
                 partion[i].setStatus(false);
                 partion[i].setProcessID(PID);
                 partion[i].CalcInternalFragment(Psize);
@@ -78,33 +75,29 @@ class Memory{
             }
         }
         return false;
-
     }
 
         private boolean WorstFit(String PID, int Psize){
+            if(PID == null) return false;
+            int worst = -1;
 
-        int worst = -1;
-
-        if(PID == null) return false;
-
-        for(int i=0; i<noOfPartitions; i++; ){
-          if(partion[i].isFree() && partion[i].getSize() >= Psize ){
-            worst = i;
-          
-          for(int j = i+1 ; j<noOfPartitions ; j++){
-             if(partion[j].isFree() && partion[j].getSize() >= Psize && partion[j].getSize() > partion[worst].getSize() ){
-            worst = j;
-             }
-          }
-          break;
-          }
-        } // end of outer loop
+            for(int i=0; i<noOfPartitions; i++ ){
+                if(partion[i].isFree() && partion[i].getSize() >= Psize ){
+                    worst = i;
+                
+                    for(int j = i+1 ; j<noOfPartitions ; j++){
+                        if(partion[j].isFree() && partion[j].getSize() >= Psize && partion[j].getSize() > partion[worst].getSize() ){
+                            worst = j;
+                        }
+                    }
+                    break;
+                }
+            } // end of outer loop
         if(worst != -1){
-
-                partion[worst].setStatus(false);
-                partion[worst].setProcessID(PID);
-                partion[worst].CalcInternalFragment(Psize);
-                 return true;
+            partion[worst].setStatus(false);
+            partion[worst].setProcessID(PID);
+            partion[worst].CalcInternalFragment(Psize);
+            return true;
         }
        return false;
 
